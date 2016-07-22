c11f091204f3be23e84f91f36bff17b21fb3ad8b
Bharat Warule <bwarule@gmail.com>
2016-07-22 20:08:42 +0530

    Create stage_dataprocess1.R

---
+++ stage_dataprocess1.R
@@ -0,0 +1,5 @@
+## stage_dataprocess1.R
+## do the univariate Regression  on each variable
+## calculate the IV, gain ratio for each variable
+## perform the stepwise Regression
+## calculate VIF for model
