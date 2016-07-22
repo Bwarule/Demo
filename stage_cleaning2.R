021dcd1cdd243ec622a3ba0e320b49ffd1507ad8
Bharat Warule <bwarule@gmail.com>
2016-07-22 19:47:36 +0530

    Create stage_cleaning2.R

---
+++ stage_cleaning2.R
@@ -0,0 +1,4 @@
+## handle the outliers and group the categorical variables for analysis
+## -- perform the outliers analysis on numeric variable
+## transform log scale variable (if required)
+## -- club the sub categories into one of the corresponding category based event rate
