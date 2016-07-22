2216c0a3f6dd946a35adff8f98bd574efc50e087
Bharat Warule <bwarule@gmail.com>
2016-07-22 19:43:12 +0530

    Create stage_cleaning1.R

---
+++ stage_cleaning1.R
@@ -0,0 +1,5 @@
+## write the missing value impute code for each variable
+## -- understand the type of each variable,
+## -- calculate the count of missing value
+## -- calculate the min, max, summary for each variable
+## -- generate the frequency table for categorical variable
