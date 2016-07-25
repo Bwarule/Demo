8578014bc1e549cb16cae9ef787f310689efe679
Bharat Warule <bwarule@gmail.com>
2016-07-25 13:19:22 +0530

    Update stage_cleaning1.R

--- stage_cleaning1.R
+++ stage_cleaning1.R
@@ -1,5 +1,7 @@
 ## write the missing value impute code for each variable
 ## -- understand the type of each variable,
 ## -- calculate the count of missing value
+NA_count <- function(x){return(sum(is.na(x)))}
+
 ## -- calculate the min, max, summary for each variable
 ## -- generate the frequency table for categorical variable
