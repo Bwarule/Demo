a14cebee3beca09cab283b90f76a7963c0687d17
Bharat Warule <bwarule@gmail.com>
2016-07-27 21:12:58 +0530

    Update stage_cleaning1.R

--- stage_cleaning1.R
+++ stage_cleaning1.R
@@ -3,11 +3,27 @@ get_class <- function(dataIn){
 	mydata <- data.frame(variables= names(dataIn),type= sapply(dataIn,class),row.names = NULL)
 	return(mydata)
 }
+# simple sapply(dataIn,class)
 
 ## -- calculate the count of missing value
 NA_count <- function(x){return(sum(is.na(x)))}
 
 ## -- calculate the min, max, summary for each variable
+
+each_summary <- function(dataIn){
+	Output <- data.frame()
+	for(i in 1:ncol(dataIn)){
+		summaryout <- 
+		data.frame(cbind(summary(dataIn[,i]),names(dataIn)[i]))
+		if(nrow(Output)<1){
+			Output <- summaryout
+		}else{
+			Output <- rbind(Output, summaryout)		
+		}
+	}
+	names(Output) <- c("value","variable")
+	return(Output)
+}
 
 ## -- generate the frequency table for categorical variable
 ## write the missing value impute code for each variable
