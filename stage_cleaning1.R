388bc6680124ceca20684f7e84a4d54195dba6e5
Bharat Warule <bwarule@gmail.com>
2016-07-28 10:13:20 +0530

    Update stage_cleaning1.R

--- stage_cleaning1.R
+++ stage_cleaning1.R
@@ -27,3 +27,94 @@ each_summary <- function(dataIn){
 
 ## -- generate the frequency table for categorical variable
 ## write the missing value impute code for each variable
+ImputationMissingvalue <- 
+function (dataIn, Method = "Mean", variable_List, MissingknoutP = 0.25) 
+{
+    library(VIM)
+    org_Method <- Method
+    a <- aggr(dataIn[,variable_List], plot = FALSE)
+    Missing_Data <- a$missings
+    rm(a);  
+	gc();
+	
+    rownames(Missing_Data) <- NULL
+    eachsummary <- data.frame()
+    Missing_Data$Percentage <- Missing_Data$Count/dim(dataIn)[1]
+	
+	if(length(variable_List)==1){
+	    Missing_Data$Variable <- variable_List
+		Missing_Data <- Missing_Data[which(Missing_Data$Variable == 
+        variable_List), ]
+	}
+    DataSeLvars <- Missing_Data[which(0 < Missing_Data$Percentage & Missing_Data$Percentage < 1), ]
+    inputation <- DataSeLvars$Variable
+	
+	dropsSeLvars <- Missing_Data[which(Missing_Data$Percentage > 
+        MissingknoutP), ]
+    drops <- dropsSeLvars$Variable
+	
+    if (length(inputation) == 0 && length(drops) == 0) {
+	    warning("There is no missing value in ", variable_List, 
+        "\n")
+		return(list(dataout = dataIn, Summary_MissingData = Missing_Data))
+    }
+        ## && length(inputation) != 0
+    if (length(drops) != 0 ) {
+		## we don't want to drop the variable flag will be rejected!
+        ## dataIn <- dataIn[, !(names(dataIn) %in% drops)]
+        cat("Variable ", drops,
+ 			"is rejected because of Max missing percentage exceeds cut-off", MissingknoutP, "\n")
+		if(length(inputation) == 0){
+			return(list(dataout = dataIn,
+						variable_drops_knout = drops,	
+						Summary_MissingData = Missing_Data))
+		}
+    }
+        
+    if (length(inputation) > 0) {
+		variable_List <- inputation
+		
+		for (i in 1:length(variable_List)) {
+			var_imputation <- variable_List[i]
+            classObj <- class(dataIn[, var_imputation])
+			
+            if (classObj == "numeric" ) {
+                 Method <- org_Method
+				if (Method == "Mean") {
+                    Mean_value <- mean(dataIn[, var_imputation], 
+                      na.rm = TRUE)
+                    dataIn[is.na(dataIn[, var_imputation]), var_imputation] <- Mean_value
+                }
+                if (Method == "TrimmedMean") {
+                    Mean_value <- mean(dataIn[, var_imputation], 
+                      na.rm = TRUE, trim = 0.1)
+                    dataIn[is.na(dataIn[, var_imputation]), var_imputation] <- Mean_value
+                }
+				if (Method == "Median") {
+                   median_value <- median(dataIn[, var_imputation], 
+                     na.rm = TRUE)
+                   dataIn[is.na(dataIn[, var_imputation]), var_imputation] <- median_value
+                }
+                if (Method == "Zero") {
+                    dataIn[is.na(dataIn[, var_imputation]), var_imputation] <- 0
+                }
+            
+			}else{
+                 Method <- "Mode"
+                 if (classObj == "factor" | classObj == "logical" | 
+                    classObj == "character" | classObj == "integer") {
+                    mode_value <- which.max(table(dataIn[, var_imputation]))
+                    dataIn[is.na(dataIn[, var_imputation]), var_imputation] <- names(mode_value)
+                  }
+                }
+                used <- cbind(variableIM = var_imputation, methodIM = Method)
+                eachsummary <- rbind(eachsummary, used)
+        }
+        
+        return(list(dataout = dataIn,
+ 			        Summary_MissingData = Missing_Data,
+ 					variable_drops_knout = drops,
+ 					Variable_Method_Imputation = eachsummary))
+    }
+
+}
